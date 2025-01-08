0b61e6cdb59ec7cd47c8617582f45e9fc4cae348
Anthony Mowry <anthonymowry18@gmail.com>
2025-01-08 17:07:17 -0500

    Exercise 1.1 Final

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,9 +10,14 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} num={exercises1}/>
-      <Content part={part2} num={exercises2}/>
-      <Content part={part3} num={exercises3}/>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
@@ -28,13 +33,20 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props)
   return (
     <div>
-      <p>{props.part} {props.num}</p>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
